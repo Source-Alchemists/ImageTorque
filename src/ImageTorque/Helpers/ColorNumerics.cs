363cbce6f861af33eec2e2b409ce6b60caf46e34
Levent Tasdemir <light4code@gmail.com>
2024-08-11 13:21:30 +0200

    Removed code duplication

--- src/ImageTorque/Helpers/ColorNumerics.cs
+++ src/ImageTorque/Helpers/ColorNumerics.cs
@@ -41,69 +41,51 @@ internal static class ColorNumerics
     public static ushort Get16BitBT709Luminance(float r, float g, float b)
         => (ushort)((r * .2126F) + (g * .7152F) + (b * .0722F) + 0.5F);
 
+    /// <summary>
+    /// Gets the saturation from the rgb component
+    /// </summary>
+    /// <param name="rgb">The rgb component.</param>
+    /// <returns>The saturation.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static float GetSaturation(Rgb rgb)
     {
         float r = rgb.Red;
         float g = rgb.Green;
         float b = rgb.Blue;
-
-        float max = MathF.Max(r, MathF.Max(g, b));
-        float min = MathF.Min(r, MathF.Min(g, b));
-        float chroma = max - min;
-
-        if (MathF.Abs(chroma) < float.Epsilon)
-        {
-            return 0F;
-        }
-
-        float l = (max + min) / 2F;
-
-        if (l <= .5F)
-        {
-            return chroma / (max + min);
-        }
-        else
-        {
-            return chroma / (2F - max - min);
-        }
+        return GetSaturation(r, g, b);
     }
 
+    /// <summary>
+    /// Gets the saturation from the rgb component
+    /// </summary>
+    /// <param name="rgb">The rgb component.</param>
+    /// <returns>The saturation.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static float GetSaturation(Rgb24 rgb)
     {
         float r = rgb.Red / 255F;
         float g = rgb.Green / 255F;
         float b = rgb.Blue / 255F;
-
-        float max = MathF.Max(r, MathF.Max(g, b));
-        float min = MathF.Min(r, MathF.Min(g, b));
-        float chroma = max - min;
-
-        if (MathF.Abs(chroma) < float.Epsilon)
-        {
-            return 0F;
-        }
-
-        float l = (max + min) / 2F;
-
-        if (l <= .5F)
-        {
-            return chroma / (max + min);
-        }
-        else
-        {
-            return chroma / (2F - max - min);
-        }
+        return GetSaturation(r, g, b);
     }
 
+    /// <summary>
+    /// Gets the saturation from the rgb component
+    /// </summary>
+    /// <param name="rgb">The rgb component.</param>
+    /// <returns>The saturation.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static float GetSaturation(Rgb48 rgb)
     {
         float r = rgb.Red / 65535F;
         float g = rgb.Green / 65535F;
         float b = rgb.Blue / 65535F;
+        return GetSaturation(r, g, b);
+    }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static float GetSaturation(float r, float g, float b)
+    {
         float max = MathF.Max(r, MathF.Max(g, b));
         float min = MathF.Min(r, MathF.Min(g, b));
         float chroma = max - min;
@@ -125,6 +107,11 @@ internal static class ColorNumerics
         }
     }
 
+    /// <summary>
+    /// Gets the max chroma from the rgb component.
+    /// </summary>
+    /// <param name="rgb">The rgb component.</param>
+    /// <returns>The max chroma.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static float GetMaxChroma(Rgb rgb)
     {
@@ -132,13 +119,15 @@ internal static class ColorNumerics
         float g = rgb.Green;
         float b = rgb.Blue;
         const float achromatic = 0.5F;
-
-        float cb = 0.501F + ((-0.168736F * r) - (0.331264F * g) + (0.5F * b));
-        float cr = 0.501F + ((0.5F * r) - (0.418688F * g) - (0.081312F * b));
-
-        return MathF.Max(MathF.Abs(cb - achromatic), MathF.Abs(cr - achromatic));
+        const float chromatic = 0.501F;
+        return GetMaxChroma(r, g, b, achromatic, chromatic);
     }
 
+    /// <summary>
+    /// Gets the max chroma from the rgb component.
+    /// </summary>
+    /// <param name="rgb">The rgb component.</param>
+    /// <returns>The max chroma.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static float GetMaxChroma(Rgb24 rgb)
     {
@@ -146,13 +135,15 @@ internal static class ColorNumerics
         float g = rgb.Green;
         float b = rgb.Blue;
         const float achromatic = 127.5F;
-
-        float cb = 128F + ((-0.168736F * r) - (0.331264F * g) + (0.5F * b));
-        float cr = 128F + ((0.5F * r) - (0.418688F * g) - (0.081312F * b));
-
-        return MathF.Max(MathF.Abs(cb - achromatic), MathF.Abs(cr - achromatic));
+        const float chromatic = 128F;
+        return GetMaxChroma(r, g, b, achromatic, chromatic);
     }
 
+    /// <summary>
+    /// Gets the max chroma from the rgb component.
+    /// </summary>
+    /// <param name="rgb">The rgb component.</param>
+    /// <returns>The max chroma.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static float GetMaxChroma(Rgb48 rgb)
     {
@@ -160,9 +151,15 @@ internal static class ColorNumerics
         float g = rgb.Green;
         float b = rgb.Blue;
         const float achromatic = 32767.5F;
+        const float chromatic = 32768F;
+        return GetMaxChroma(r, g, b, achromatic, chromatic);
+    }
 
-        float cb = 32768F + ((-0.168736F * r) - (0.331264F * g) + (0.5F * b));
-        float cr = 32768F + ((0.5F * r) - (0.418688F * g) - (0.081312F * b));
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static float GetMaxChroma(float r, float g, float b, float achromatic, float chromatic)
+    {
+        float cb = chromatic + ((-0.168736F * r) - (0.331264F * g) + (0.5F * b));
+        float cr = chromatic + ((0.5F * r) - (0.418688F * g) - (0.081312F * b));
 
         return MathF.Max(MathF.Abs(cb - achromatic), MathF.Abs(cr - achromatic));
     }
